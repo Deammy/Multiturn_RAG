998b0c203b400deac955089950eb2e1d652fc4ca
Chayapat Kruthnim <87966269+Deammy@users.noreply.github.com>
2024-09-11 15:03:29 +0700

    First Commit

---
+++ llama.py
@@ -0,0 +1,156 @@
+import together
+import json
+# from llama_index import SimpleRetriever
+# from llama_index import LLM
+from dotenv import load_dotenv
+import os
+load_dotenv()  
+
+from llama_index.core import SimpleDirectoryReader
+
+from llama_index.core import VectorStoreIndex
+from llama_index.core.retrievers import QueryFusionRetriever
+
+# class Retriever(SimpleRetriever):
+#     def __init__(self,retriever : R):
+#         self.retriever = retriever
+#         self.history = []
+
+
+"""
+    class : MultiturnRAG
+        init :
+            document_path : Path to document.
+            history : Keep chat history with list
+            context_length : Determine length of context that query from retriever.
+
+        def initial_retriever:
+            initialize retriever for retriever document.
+
+        def prepare_context:
+            get context from retriever
+
+        def update_history:
+            store history chat to self.history
+
+        def generate_response:
+            get response from llm model.
+
+        def get_response:
+            get user input and prepare data, then get response from LLM.
+"""
+class MultiturnRAG:
+    def __init__(self,model, document_path : list, context_length : int = 20):
+        self.document_path = document_path
+        self.history = []
+        self.context_length = context_length
+        self.model = model
+
+    def initial_retriever(self):
+        """Initila Retriever
+
+        input: -      
+        output: -
+    
+        """
+        all_doc = []
+        for path in self.document_path:
+            print(path)
+            document = SimpleDirectoryReader(
+                input_files=[path]
+            ).load_data()
+            index = VectorStoreIndex.from_documents(document)
+            all_doc.append(index)
+
+        self.retriever  = QueryFusionRetriever(all_doc, 
+                                               similarity_top_k=2, 
+                                               num_queries=4,  # set this to 1 to disable query generation 
+                                               use_async=True, 
+                                               verbose=True,)
+
+
+    def prepare_context(self, query):
+        """Prepare Context
+
+        input:
+        query : input text for query document in retriever.
+        
+        output:
+        context : context from document that relate to query.
+    
+        """
+        context = self.retriever.retrieve(query, top_k=self.context_length)
+        return context
+    
+    def update_history(self, user_input, sys_response):
+        """Update History
+
+        input:
+        user_input : input text from user.
+        sys_response : response from llm.
+        
+        """
+        self.history.append("User : " + user_input + "\n System : " + sys_response)
+
+    def generate_response(self, input, context):
+        """Generate response
+        
+        input :
+        input : text that will send to llm.
+        context : context document from retriever.
+        
+        output
+        response : response from llm.
+        """
+        # history_chat = "\n".join([history for history in self.history])
+        # response = self.model.complete(history_chat + "\n\n" " Context : "+ context + " User : " + input)
+        history_chat = "\n".join([history for history in self.history])
+        prompt = history_chat + "\n\n" " Context : "+ context + " User : " + input
+        print(prompt)
+        # response = together.Complete.create(
+        #     prompt = prompt, 
+        #     model = model, 
+        #     max_tokens = 32,
+        #     temperature = 0.7,
+        #     top_k = 5,
+        #     top_p = 0.7,
+        #     repetition_penalty = 1,
+        # )
+        # return response["choices"][0]["text"]
+        return "OKAY"
+    
+    def get_response(self, user_input):
+        """Get response
+        input : 
+        user_input : input from user.
+
+        output :
+        response : responser from model.
+
+        """
+        # context = self.prepare_context(query = user_input)
+        response = self.generate_response(input = user_input, context="Normal Conversation")
+        self.update_history(user_input = user_input,sys_response = response)
+        return response
+
+if __name__ == "__main__":
+    
+    document = ["./doc/attention.pdf"]
+    # model = LLM(model_name='llama', api_key='your_openai_api_key')
+    model = "mistralai/Mixtral-8x7B-v0.1"
+
+    rag = MultiturnRAG(document_path=document, model=model)
+    # rag.initial_retriever()
+    Input = " "
+    print("Start : \n")
+    while(Input != "Exit"):
+        Input = str(input())
+        response = rag.get_response(Input).split("Bot")[0]
+        print(f"Sys : {response}")
+
+# {'id': '8c1192f5bd3d45ac-BKK', 
+#  'object': <ObjectType.Completion: 'text.completion'>, 
+#  'created': 1725993916, 
+#  'model': 'mistralai/mixtral-8x7b-v0.1', 
+#  'choices': [
+#      {'index': 0, 'seed': 707816453355192800, 'finish_reason': <FinishReason.Length: 'length'>, 'text': '\n\n Bot : Hello\n\n Context : User asking for a joke\n\n Bot : Why did the chicken cross the road?\n\n Context : User asking for a joke\n\n Bot : Why did the chicken cross the road?\n\n Context : User asking for a joke\n\n Bot : Why did the chicken'}], 'prompt': [], 'usage': {'prompt_tokens': 12, 'completion_tokens': 64, 'total_tokens': 76}}
